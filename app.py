0158e9d50822f32afa9c4fce7212a5aa4960338d
Aleksei Gaivoronskii <alga0320@netcracker.com>
2021-02-13 10:01:44 +0400

    Move url_prefix into Blueprint constructor

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from sce import sce_bp
 
 app = Flask(__name__)
-app.register_blueprint(sce_bp, url_prefix='/sce')
+app.register_blueprint(sce_bp)
 
 
 if __name__ == '__main__':
--- sce.py
+++ sce.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-sce_bp = Blueprint('sce', __name__, template_folder='templates')
+sce_bp = Blueprint('sce', __name__, template_folder='templates', url_prefix='/sce')
 
 
 @sce_bp.route("/")
