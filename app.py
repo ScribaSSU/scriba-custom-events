99e0412d96e07c330fb5b59b6930a2ec770d6dbe
Aleksei Gaivoronskii <alga0320@netcracker.com>
2021-02-20 00:24:10 +0400

    The great changes has come

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
-from flask import Flask
-from sce import sce_bp
-from settings import config
-
 import logging
 
-app = Flask(__name__)
-app.register_blueprint(sce_bp)
+from flask import Flask
 
-logging.basicConfig(level=config['logging']['level'])
+from database import db_session
+from settings import log_conf
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
+
+
+def configure_logs():
+    log_file_name = log_conf['file_name']
+    log_level = log_conf['level']
+
+    # Needed to clear log file
+    with open(log_file_name, 'w'):
+        pass
+
+    logging.basicConfig(filename=log_file_name, level=log_level,
+                        format='%(asctime)s %(levelname)-5s[%(name)s] - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+
 
 if __name__ == '__main__':
+    configure_logs()
     app.run()
--- settings.py
+++ settings.py
@@ -2,3 +2,5 @@ import yaml
 
 with open('config.yaml') as file:
     config = yaml.safe_load(file)
+db_conf = config['database']
+log_conf = config['logging']
