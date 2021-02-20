1083e2350d886f80a706ed28f2e7828156769008
NadyaSukhova <suhova1313@gmail.com>
2021-02-20 23:59:46 +0400

    Used Black to change files formatting, fixed buttons forms

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 
 class Event(Base):
-    __tablename__ = 'events'
+    __tablename__ = "events"
     event_id = Column(Integer, primary_key=True)
     user_id = Column(Integer, nullable=False)
     event_name = Column(String(200), nullable=False)
@@ -12,12 +12,14 @@ class Event(Base):
     date = Column(DateTime, default=datetime.utcnow)
     repeat = Column(Boolean, default=False)
 
-    def __init__(self,
-                 user_id: int,
-                 event_name: String,
-                 description: String = None,
-                 date: datetime = None,
-                 repeat: bool = None):
+    def __init__(
+        self,
+        user_id: int,
+        event_name: String,
+        description: String = None,
+        date: datetime = None,
+        repeat: bool = None,
+    ):
         self.user_id = user_id
         self.event_name = event_name
         if description is not None:
@@ -28,4 +30,4 @@ class Event(Base):
             self.repeat = repeat
 
     def __repr__(self):
-        return f'<Event {self.event_id}>'
+        return f"<Event {self.event_id}>"
--- routes.py
+++ routes.py
@@ -2,49 +2,60 @@ from flask import render_template, request, redirect
 
 from app import app
 
-client_id = 12221 # айди приложения ВК, заменить, когда будет зарегистрировано, и вынести в конфиг
-client_secret = 211112 # секретный ключ приложения ВК, заменить, когда будет зарегистрировано, и вынести в конфиг
-redirect_uri = 'http://scriba-custom-events.ru/vk_auth'
+client_id = 12221  # айди приложения ВК, заменить, когда будет зарегистрировано, и вынести в конфиг
+client_secret = 211112  # секретный ключ приложения ВК, заменить, когда будет зарегистрировано, и вынести в конфиг
+redirect_uri = "http://scriba-custom-events.ru/vk_auth"
+
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route("/classes", methods=['GET', 'POST'])
+@app.route("/classes", methods=["GET", "POST"])
 def classes():
-    if request.method == 'GET':                     # список пар
-        return render_template('index.html')
-    elif request.method == 'POST':                  # добавить пару
-        return render_template('index.html')
+    if request.method == "GET":  # список пар
+        return render_template("index.html")
+    elif request.method == "POST":  # добавить пару
+        return render_template("index.html")
 
 
-@app.route("/events", methods=['GET', 'POST'])
+@app.route("/events", methods=["GET", "POST"])
 def events():
-    if request.method == 'GET':                     # список кастомных событий
-        return render_template('custom_events.html')
-    elif request.method == 'POST':                  # добавить кастомное событие
-        return render_template('custom_events.html')
+    if request.method == "GET":  # список кастомных событий
+        return render_template("custom_events.html")
+    elif request.method == "POST":  # добавить кастомное событие
+        return render_template("custom_events.html")
 
 
 @app.route("/login")
 def login():
-    return redirect('https://oauth.vk.com/authorize?client_id={}&display=page&redirect_uri={}&response_type=code'.format(client_id, redirect_uri))
+    return redirect(
+        "https://oauth.vk.com/authorize?client_id={}&display=page&redirect_uri={}&response_type=code".format(
+            client_id, redirect_uri
+        )
+    )
 
-@app.route("/vk_auth", methods=['GET', 'POST'])
+
+@app.route("/vk_auth", methods=["GET", "POST"])
 def vk_auth():
-    if request.method == 'GET':
-        auth_code = request.args.get('code')
+    if request.method == "GET":
+        auth_code = request.args.get("code")
         if not auth_code:
-            return redirect(url_for('index'))
-        return redirect('https://oauth.vk.com/access_token?client_id={}&client_secret={}&redirect_uri={}&code={}'.format(client_id, client_secret, redirect_uri, auth_code))
-    elif request.method == 'POST':
-        if request.form.get('error'):
-            return redirect(url_for('index'))
-        access_token = request.form.get('access_token')
-        user_id = request.form.get('user_id')
-        return redirect(url_for('index'))
+            return redirect(url_for("index"))
+        return redirect(
+            "https://oauth.vk.com/access_token?client_id={}&client_secret={}&redirect_uri={}&code={}".format(
+                client_id, client_secret, redirect_uri, auth_code
+            )
+        )
+    elif request.method == "POST":
+        if request.form.get("error"):
+            return redirect(url_for("index"))
+        access_token = request.form.get("access_token")
+        user_id = request.form.get("user_id")
+        return redirect(url_for("index"))
+
 
 @app.route("/logout")
 def logout():
-    return render_template('index.html')
+    return render_template("index.html")
