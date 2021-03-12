3931c7824c1e188faae379e2d4f4c5e6375c8103
NadyaSukhova <suhova1313@gmail.com>
2021-03-13 00:18:31 +0400

    changed event.py

--- models/event.py
+++ models/event.py
@@ -9,7 +9,7 @@ class Event(db.Model):
     event_name = db.Column(db.String(200), nullable=False)
     description = db.Column(db.Text)
     date_begin = db.Column(db.DateTime, default=datetime.now, nullable=False)
-    date_end = db.Column(db.DateTime, default=datetime.now)
+    date_end = db.Column(db.DateTime, default=date_begin)
     repeat = db.Column(db.String(200), nullable=False)
 
     def __repr__(self):
