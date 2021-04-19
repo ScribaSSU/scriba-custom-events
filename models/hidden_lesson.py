7e25990ed244e30979735aa18204f571fb93a608
Chari <derov.vasily@gmail.com>
2021-04-19 14:39:36 +0400

    added lesson hiding functionality, some adjustments to tracto requests performance, minor code refactoring, additional input check in custom event submission

---
+++ models/hidden_lesson.py
@@ -0,0 +1,53 @@
+from datetime import datetime
+from models.db import db
+
+
+class HiddenLesson(db.Model):
+    __tablename__ = "addedLesson"
+    hidden_lesson_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, nullable=False)
+    department = db.Column(db.String(200), nullable=False)
+    group = db.Column(db.String(200), nullable=False)
+    sub_group = db.Column(db.String(200), nullable=False)
+    week_day = db.Column(db.Integer, nullable=False)
+    week_type = db.Column(db.String(200), nullable=False)
+    lesson_number = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"<HiddenLesson {self.hidden_lesson_id}>"
+
+    @staticmethod
+    def find_all():
+        return db.session.query(HiddenLesson).all()
+
+    @staticmethod
+    def find_hidden_lessons(user_id):
+        return db.session.query(HiddenLesson).filter_by(user_id=user_id).all()
+
+    @staticmethod
+    def find_hidden_lessons_by_day(user_id, week_day):
+        return db.session.query(HiddenLesson).filter_by(user_id=user_id, week_day=week_day).all()
+
+    @staticmethod
+    def hide_lesson(user_id, user_department, user_group, sub_group, week_day, week_type, lesson_number):
+        new_hidden_lesson = HiddenLesson(
+            user_id=user_id,
+            department=user_department,
+            group=user_group,
+            sub_group=sub_group,
+            week_day=week_day,
+            week_type=week_type,
+            lesson_number=lesson_number)
+        db.session.add(new_hidden_lesson)
+        db.session.commit()
+        return new_hidden_lesson.hidden_lesson_id
+
+    @staticmethod
+    def unhide_lesson(lesson_id):
+        db.session.query(HiddenLesson).filter_by(hidden_lesson_id=lesson_id).delete()
+        db.session.commit()
+
+    @staticmethod
+    def clear_lessons(user_id):
+        db.session.query(HiddenLesson).filter_by(user_id=user_id).delete()
+        db.session.commit()
--- models/user_preferences.py
+++ models/user_preferences.py
@@ -4,9 +4,10 @@ from models.db import db
 class UserPreferences(db.Model):
     __tablename__ = "preferences"
     user_id = db.Column(db.Integer, nullable=False, primary_key=True)
-    user_department = db.Column(db.String(200))
-    user_course = db.Column(db.String(200))
-    user_group = db.Column(db.String(200))
+    user_department = db.Column(db.String(200), nullable=False)
+    user_course = db.Column(db.String(200), nullable=False)
+    user_group = db.Column(db.String(200), nullable=False)
+    show_hidden = db.Column(db.Boolean, nullable=False)
 
     def __repr__(self):
         return f"<Факультет {self.user_department}, группа {self.user_group}>"
@@ -16,15 +17,23 @@ class UserPreferences(db.Model):
         return db.session.query(UserPreferences).all()
 
     @staticmethod
-    def get_user_group(vk_id=2):
-        return db.session.query(UserPreferences).filter_by(user_id=vk_id).all()
+    def get_user_preferences(user_id=2):
+        return db.session.query(UserPreferences).filter_by(user_id=user_id).first()
 
     @staticmethod
-    def save_user_group(user_id, user_department, user_course, user_group):
+    def update_show_preferences(user_id=2):
+        user = db.session.query(UserPreferences).filter_by(user_id=user_id).first()
+        if user:
+            user.show_hidden = not user.show_hidden
+            db.session.commit()
+
+    @staticmethod
+    def save_user_preferences(user_id, user_department, user_course, user_group, show_hidden=False):
         new_preference = UserPreferences(
             user_id=user_id,
             user_department=user_department,
             user_course=user_course,
-            user_group=user_group)
+            user_group=user_group,
+            show_hidden=show_hidden)
         db.session.merge(new_preference)
         db.session.commit()
